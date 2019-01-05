083c09d2232380984a1c75cbdb914a71dcb399aa
RayCJ87 <shihcr.ke@gmail.com>
2019-01-05 21:19:47 +0000

    Sum Array of Numbers finished

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let ans = 0
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])){
+      ans += sumItems(array[i])
+    }
+    else {
+      ans+= array[i];
+    }
+  }
+  return ans;
 }
+
 
 module.exports = sumItems;
